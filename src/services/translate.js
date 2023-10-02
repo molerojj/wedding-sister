4e214e985b0dbd6030f210fa24bd721e208fc868
Jonathan Molero <110490630+molerojj@users.noreply.github.com>
2023-10-02 13:55:30 -0400

    fix margin

---
+++ src/services/translate.js
@@ -0,0 +1,65 @@
+// NAVBAR
+
+const item1 = ['Our Wedding', 'Nuestra Boda']
+const item2 = ['The Venue', 'El lugar']
+const item3 = ['Itinerary', 'Itinerario']
+const item4 = ['Gifts', 'Regalos']
+const item5 = ['Dress Code', 'Codigo de Vestimenta']
+const item6 = ['RSVP', 'Conrifmacion']
+
+// HERO SECTION
+
+const betterTogether = ['Better Together', 'Mejor Juntos']
+const fecha = ['December 15, 2023', '15 de Diciembre de 2023']
+const fraseH = ['Two are better than one, and even more united with Christ, three-fold cord difficult to break.', 'Mejor son dos que uno, y aun mas unidos con Cristo, cordon de tres dobleces dificil de romper.']
+
+// OUR WEDDING
+
+const titleOW = ['OUR WEDDING', 'NOS CASAMOS']
+
+const parrafoOW = ['We are happy to invite you to celebrate with us a day full of love where we will celebrate our union. We know that your company will fill us with joy and will come with the best wishes.', 'Estamos felices de invitarte a celebrar con nosotros un día lleno de amor donde celebraremos nuestra unión. Sabemos que tu compañia nos llenará de alegría y vendrá con los mejores deseos.']
+
+const fechaOW = [{
+    english: ['Days', 'Hours', 'Min', 'Sec'],
+    espanol: ['Dias', 'Horas', 'Min', 'Seg'],
+}]
+
+const fraseOW = ['The adventure begining...', 'La aventura comienza...']
+
+// THE VENUE
+
+//titulo = item 2
+
+const buttonTV = ['See the venue', 'Conoce el lugar']
+const parking = ['Parking', 'Estacionamiento']
+const price = ['Price', 'Precio', 'Cash or Card', 'Efectivo o Tarjeta']
+const reservaHotel = ['Booking Hotel', 'Reserva en Hotel', 'Web Page', 'Pagina Web']
+
+// ITINERARY
+
+// titulo = item 3
+
+const itiENG = ['WELCOME', 'CEREMONY', 'COCTAIL', 'RECEPTION', 'DINNER']
+const itiESP = ['BIENVENIDA', 'CEREMONIA', 'COCTEL', 'RECEPCION', 'CENA']
+const data = ['The ceremony start 4:00PM oclock arrive on time and enjoy full.', 'La ceremonia inica a las 4:00PM, llega a tiempo y disfruta a pleno']
+
+// REGALOS
+
+// titulo = item 4
+
+// DRESS CODE
+
+// titulo = item 5
+
+const fraseG = ['Dress your best smile', 'Luce tu mejor sonrisa']
+
+//RSVP
+
+const frase1 = ['We will be happy to see you there.', 'Estaremos felices de verte alli']
+const button = ['RSVP', 'CONFIRMAR']
+const textRSVP = ['In God who is almighty to do everything beyond what we imagine, Through Him in us, be glory in this marriage in Christ for all generations. Amen.', 'En Dios que es todopoderoso para hacer todo más allá de lo que imaginamos, por Él en nosotros, sea la gloria en este matrimonio en Cristo por todas las generaciones. Amén.']
+const lib = ['Ephesians', 'Efesios']
+
+// FOOTER
+
+const NM = ['MADE BY', 'HECHO POR']
