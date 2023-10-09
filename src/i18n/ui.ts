ecb789937835104226e08705a19d2c602495fa78
Jonathan Molero <110490630+molerojj@users.noreply.github.com>
2023-10-09 19:19:07 -0400

    add translate en-es

---
+++ src/i18n/ui.ts
@@ -0,0 +1,145 @@
+export const languages = {
+    en: 'EN 🇺🇸',
+    es: 'ES 🇪🇸',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        // navbar
+        'nav.item1': 'Our Wedding',
+        'nav.item2': 'The Venue',
+        'nav.item3': 'Itinerary',
+        'nav.item4': 'Gifts',
+        'nav.item5': 'Dress Code',
+        'nav.item6': 'RSVP',
+        // about section
+        'about.title': 'ABOUT US',
+        'about.t1': 'Jack Excel Yeldy PowerPoint',
+        'about.t2': 'Jack morning person Yeldy after 10am',
+        'about.t3': 'Jack no bun Yeldy arepa with cheese',
+        'about.t4': 'Jack text msg Yeldy Videocall',
+        'about.t5': 'Jack rare Yeldy well done',
+        'about.t6': 'Jack Facebook Yeldy Instagram',
+        'about.parragraph': 'We love family, traveling, eating, planning, lying on the couch and sharing (well more me than Him but now we are one hahaha 😄)',
+        'about.phrase1': 'We love because God loved us first',
+        'about.phrase2': 'We love because God',
+        'about.phrase3': 'loved us first',
+        // hero section
+        'hero.title': 'Better Together',
+        'hero.date': 'December 15, 2023',
+        'hero.phrase1': 'Two are better than one, and even more united with Christ,',
+        'hero.phrase2': 'three-fold cord difficult to break',
+        // our wedding section
+        'our.title': 'OUR WEDDING',
+        'our.paragraph': 'We are happy to invite you to celebrate with us a day full of love where we will celebrate our union. We know that your company will fill us with joy and will come with the best wishes.',
+        'our.day': 'Days',
+        'our.hour': 'Hours',
+        'our.min': 'Min',
+        'our.sec': 'Sec',
+        'our.phrase': 'The adventure begining...',
+        // venue section
+        'venue.title': 'The Venue',
+        'venue.button1': 'See the venue',
+        'venue.park': 'PARKING',
+        'venue.book': 'BOOKING HOTEL',
+        'venue.price': 'Price $10',
+        'venue.pay': 'Cash or Card',
+        'venue.web': 'Web Page',
+        // itinerary section
+        'iti.title': 'ITINERARY',
+        'iti.item1': 'WELCOME',
+        'iti.item2': 'CEREMONY',
+        'iti.item3': 'COCTAIL',
+        'iti.item4': 'RECEPTION',
+        'iti.item5': 'DINNER',
+        'iti.note1': 'The ceremony start 4:00PM oclock',
+        'iti.note2': 'arrive on time and enjoy full',
+        // gift section
+        'gift.title': 'Gifts',
+        'gift.text1': 'That you are part of our big day and we will celebrate it together is our greatest gift. If you want to give an additional',
+        'gift.text2': 'CASH GIFT',
+        'gift.text3': 'that would be great!',
+        // dress code section
+        'dress.title': 'DRESS CODE',
+        'dress.phrase': 'Dress your best smile',
+        // RSVP section
+        'rsvp.title': 'RSVP',
+        'rsvp.text': 'We will be happy to see you there',
+        'rsvp.button': 'RSVP',
+        'rsvp.phrase1': 'In God who is almighty',
+        'rsvp.phrase2': 'to do everything beyond what we imagine, Through Him in us, be glory in this marriage in Christ for all generations. Amen.',
+        'rsvp.bible': 'Ephesians',
+        // footer
+        'footer.made': 'MADE BY'
+    },
+    es: {
+        // navbar
+        'nav.item1': 'La Boda',
+        'nav.item2': 'El lugar',
+        'nav.item3': 'Itinerario',
+        'nav.item4': 'Regalos',
+        'nav.item5': 'Codigo de Vestimenta',
+        'nav.item6': 'Confirmación',
+        // about section
+        'about.title': 'CONOCENOS',
+        'about.t1': 'Jack Excel Yeldy PowerPoint',
+        'about.t2': 'Jack persona de mañana Yeldy despues de las 10am',
+        'about.t3': 'Jack nada de harina Yeldy arepa con queso',
+        'about.t4': 'Jack mensaje de texto Yeldy Videollamada',
+        'about.t5': 'Jack extraño Yeldy bien hecho',
+        'about.t6': 'Jack Facebook Yeldy Instagram',
+        'about.parragraph': 'Amanos a la familia, viajar, comer, planificar, tumbarnos en el sofá y compartir (bueno más a mí que a Él pero ahora somos uno jajaja 😄)',
+        'about.phrase1': 'Amamos porque Dios nos amó primero',
+        'about.phrase2': 'Amamos porque Dios',
+        'about.phrase3': 'nos amó primero',
+        // hero section
+        'hero.title': 'Better Together',
+        'hero.date': '15 de Diciembre de 2023',
+        'hero.phrase1': "Mejor son dos que uno, y aun mas unidos con Cristo,",
+        'hero.phrase2': "cordon de tres dobleces dificil de romper",
+        // our wedding section
+        'our.title': 'NOS CASAMOS',
+        'our.paragraph': 'Estamos felices de invitarte a celebrar con nosotros un día lleno de amor donde celebraremos nuestra unión. Sabemos que tu compañia nos llenará de alegría y vendrá con los mejores deseos.',
+        'our.day': 'Dias',
+        'our.hour': 'Horas',
+        'our.min': 'Min',
+        'our.sec': 'Seg',
+        'our.phrase': 'La aventura comienza...',
+        // venue section
+        'venue.title': 'El lugar',
+        'venue.button1': 'Ver el lugar',
+        'venue.park': 'ESTACIONAMIENTO',
+        'venue.book': 'RESERVA EN HOTEL',
+        'venue.price': 'Precio $10',
+        'venue.pay': 'Efectivo o tarjeta',
+        'venue.web': 'Pagina Web',
+        // itinerary section
+        'iti.title': 'ITINERARIO',
+        'iti.item1': 'BIENVENIDA',
+        'iti.item2': 'CEREMONIA',
+        'iti.item3': 'PRE-RECEPCIÓN',
+        'iti.item4': 'RECEPCIÓN',
+        'iti.item5': 'CENA',
+        'iti.note1': 'La ceremonia inica a las 4:00PM,',
+        'iti.note2': 'llega a tiempo y disfruta a pleno',
+        // gift section
+        'gift.title': 'Regalos',
+        'gift.text1': 'Que formes parte de nuestro gran día y lo celebraremos juntos es nuestro mayor regalo. Si desea dar, un',
+        'gift.text2': 'REGALO EN EFECTIVO',
+        'gift.text3': 'adicional, ¡sería genial!',
+        // dress code section
+        'dress.title': 'CÓDIGO DE VESTIMENTA',
+        'dress.phrase': 'Luce tu mejor sonrisa',
+        // RSVP section
+        'rsvp.title': 'Confirmación',
+        'rsvp.text': 'Estaremos felices de verte allí',
+        'rsvp.button': 'Confirmar',
+        'rsvp.phrase1': 'En Dios que es todopoderoso',
+        'rsvp.phrase2': 'para hacer todo más allá de lo que imaginamos, por Él en nosotros, sea la gloria en este matrimonio en Cristo por todas las generaciones. Amén.',
+        'rsvp.bible': 'Efesios',
+        // footer
+        'footer.made': 'HECHO POR'
+    },
+} as const;
